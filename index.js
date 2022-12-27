1e09ccda40d145eb91654c7c77acae93325d1e51
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2022-12-27 15:15:13 +0300

    Training JS #10: loop statement --for

--- index.js
+++ index.js
@@ -16,3 +16,19 @@ const padIt = (str, n) => {
 	}
 	return newSrtTwo + newSrtOne;
 };
+
+// Next kata
+
+const pickIt = (arr) => {
+	let odd = [],
+		even = [];
+	for (let i = 0; i < arr.length; i++) {
+		if ((arr[i] %= 0)) {
+			even.push(arr[i]);
+		}
+		if (arr[i] % 2 !== 0) {
+			odd.push(arr[i]);
+		}
+	}
+	return [odd, even];
+};
