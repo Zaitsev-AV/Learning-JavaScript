ea7562355f8aa51a32938cbb7004b69226c4b77d
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2022-12-30 15:23:44 +0300

    Kata Are they the "same"?

--- index.js
+++ index.js
@@ -175,3 +175,13 @@ function numberJoy(n) {
 function maxBisectors(n) {
 	return Math.round(Math.pow(n / 2, 2));
 }
+// Next kata Are they the "same"?
+function comp(a1, a2) {
+	if (a1 === null || a2 === null) return false;
+	for (let i = 0; i < a2.length; i++) {
+		const ind = a1.indexOf(Math.sqrt(a2[i]));
+		if (ind == -1) return false;
+		a1[ind] = null;
+	}
+	return true;
+}
