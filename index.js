cfacad5ffb0cd0186fe37d6a71967c3c308b0324
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2022-12-30 15:26:46 +0300

    Kata Quarter of the year

--- index.js
+++ index.js
@@ -202,4 +202,21 @@ const min = (list) => {
 		 if (list[i] > max) max = list[i];
 	 }   
 	  return max;
+}
+ 
+// Next kata Quarter of the year
+
+const quarterOf = (month) => {
+	switch (month) {
+		case 1: case 2: case 3:
+			return 1
+		case 4: case 5: case 6:
+			return 2
+		case 7: case 8: case 9:
+			return 3
+		case 10: case 11: case 12:
+			return 4
+		default:
+			break;
+	}
  }
