143696d8d8b785fa186c0e2761e61d6f36b8b00e
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2022-12-31 13:04:09 +0300

    Kata Training JS #10: loop statement --for

--- index.js
+++ index.js
@@ -296,3 +296,17 @@ const maX = (list) => {
 	}
 	return max;
 };
+
+// Next kata Training JS #10: loop statement --for
+
+const pickIt = (arr) => {
+   let odd=[],even=[];
+   for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 != 0) {
+         odd.push(arr[i])
+      } else {
+        even.push(arr[i])
+      }
+   }
+   return [odd,even];
+}
