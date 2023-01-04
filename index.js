04dd10599fece99de25bef853a89f439953d1956
Alexandr Zaitsev <zaitsev.av.12@gmail.com>
2023-01-04 13:38:44 +0300

    Kata Last digit of a large number

--- index.js
+++ index.js
@@ -350,3 +350,6 @@ function repeatStr(n, s) {
 	}
 	return str;
 }
+
+//Next kata Last digit of a large number
+const lastDigit =(str1, str2) => +!+str2 || Math.pow(str1.slice(-1), str2.slice(-2) % 4 || 4) %10
